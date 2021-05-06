249960517e4b1a4a3635d561aa61c1a009c50cf3
Tomoko <haisaitomoco@gmail.com>
2021-05-06 10:40:10 +0300

    add service component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
@@ -7,6 +8,7 @@ import { PropertyListComponent } from './property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HousingService } from './services/housing.service';
 
 @NgModule({
   declarations: [
@@ -17,9 +19,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
    ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    HousingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
