5942dd4bd2ebf0badb800a6dd3992f96adf1d27b
Tomoko <haisaitomoco@gmail.com>
2021-05-07 15:55:36 +0300

    edit add-property component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
@@ -36,6 +37,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
